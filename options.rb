ba80657e8058dcf2161d3c36f7cf20c5151d7cca
Alessandro <4143332+AlessandroMinali@users.noreply.github.com>
2021-01-02 13:21:40 -0800

    opt adjust

--- options.rb
+++ options.rb
@@ -4,17 +4,11 @@ require 'optparse'
 
 OPTIONS = {} # rubocop:disable Style/MutableConstant
 OptionParser.new do |opts|
-  opts.banner = 'Usage: riscv.rb -Eh BINARY_FILE'
+  opts.banner = 'Usage: riscv.rb -Eh FILE.bin'
 
-  opts.on('-E', 'Run with RV32E') do
+  opts.on('-E', 'Run in embedded mode (ie. RV32E)') do
     OPTIONS[:embedded] = true
   end
-
-  # opts.on('-mBITS', '--mode=BITS', 'Run in BITS mode (default: 64)') do |v|
-  #   raise 'Must include BITS for mode.' unless [32, 64, 128].include?(v.to_i)
-
-  #   OPTIONS[:mode] = v.to_i
-  # end
 
   opts.on('-h', '--help', 'Prints this help') do
     puts opts
