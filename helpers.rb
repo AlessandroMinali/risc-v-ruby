802121b7b403ddc919caf3ab24d9b0295582ceb7
Alessandro <4143332+AlessandroMinali@users.noreply.github.com>
2021-01-01 17:52:21 -0800

    simplify dump for just 32 bit

--- helpers.rb
+++ helpers.rb
@@ -55,22 +55,20 @@ end
 
 def dump_registers
   REG.each.with_index do |i, index|
-    print format("x%02<reg>d: %##{(XLEN / 3.25).round}<val>x ", reg: index, val: i)
+    print format('x%02<reg>d: %#10<val>x ', reg: index, val: i)
     print "\n" if ((index + 1) % 4).zero?
   end
 end
 
-def dump_memory # rubocop:disable Metrics/AbcSize
-  size = XLEN / 8
-  MEM.each_slice(size).with_index do |i, index|
+def dump_memory
+  MEM.each_slice(4).with_index do |i, index|
     next if i.nil?
 
-    i += Array.new(size - i.size) { 0 } if i.size < size
+    i += Array.new(4 - i.size) { 0 } if i.size < 4
     i.map!(&:to_i)
-    value = i[0] | i[1] << 8 | i[2] << 16 | i[3] << 24 if size >= 4
-    value |= i[4] << 32 | i[5] << 40 | i[6] << 48 | i[7] << 56 if size == 8
+    value = i[0] | i[1] << 8 | i[2] << 16 | i[3] << 24
 
-    print format("%##{(XLEN / 3.25).round}<mem>x: %##{(XLEN / 3.25).round}<val>x ", mem: index * size, val: value)
+    print format('%#10<mem>x: %#10<val>x ', mem: index * 4, val: value)
     print "\n" if ((index + 1) % 4).zero?
   end
 end
